4d7334974e3921c340efe054e8309ab6271f4b72
SeanIFitch <seanifitch@gmail.com>
2024-02-20 19:02:30 -0500

    removed extraneous files

--- icarus_v2 py/Module.py
+++
@@ -1,8 +0,0 @@
-class Module:
-    def __init__(self, buffer_reader) -> None:
-        self.buffer_reader = buffer_reader
-
-    def read(self):
-        while(True):
-            data = self.buffer_reader.read()
-            print(data)
