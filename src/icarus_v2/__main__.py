72db4be316550b748cace2754c067d1b0fb1f079
Sean Fitch <seanifitch@gmail.com>
2024-10-04 16:26:20 -0400

    Use Importlib for resources

--- src/icarus_v2/__main__.py
+++ src/icarus_v2/__main__.py
@@ -1,29 +1,22 @@
+import importlib
+
 from icarus_v2.backend.configuration_manager import ConfigurationManager
 from icarus_v2.gui.main_window import MainWindow
 from icarus_v2.backend.data_handler import DataHandler
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
-from icarus_v2.utils.path_utils import get_base_directory
-import os
 from icarus_v2.qdarktheme.load_style import load_stylesheet
 
 # Application entry point
 def main():
-
-    # Use XCB platform for Qt
-    # Wayland does not render drop shadows or toolbar colors and offsets popups
-    # os.environ["QT_QPA_PLATFORM"] = "xcb"
-
-    base_dir = get_base_directory()
-    config_file_path = os.path.join(base_dir, "settings.json")
-    icon_path = os.path.join(base_dir, 'resources', 'wing.png')
-
-    config_manager = ConfigurationManager(config_file_path)
+    config_manager = ConfigurationManager()
 
     app = QApplication([])
     app.setStyleSheet(load_stylesheet(theme='dark'))
-    app.setWindowIcon(QIcon(icon_path))
     app.setApplicationName("Icarus")
+
+    with importlib.resources.path('icarus_v2.resources', 'wing.png') as icon_path:
+        app.setWindowIcon(QIcon(str(icon_path)))
 
     window = MainWindow(config_manager)
     window.showMaximized()
