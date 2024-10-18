785cf5b276314bcd0c87a14ca768dfa4af1de11b
Anthony Shaw <anthonyshaw1714@gmail.com>
2024-10-18 17:05:32 -0400

    Added export functions for the graphs

---
+++ src/icarus_v2/backend/custom_csv_exporter.py
@@ -0,0 +1,101 @@
+import csv
+import itertools
+
+import numpy as np
+
+import pyqtgraph as pg
+from pyqtgraph.graphicsItems import ErrorBarItem, PlotItem
+# from ..parametertree import Parameter
+from PySide6.QtCore import QCoreApplication
+
+# from ..Qt import QtCore
+import pyqtgraph.exporters
+
+translate = QCoreApplication.translate
+
+# __all__ = ['CustomCSVExporter']
+    
+    
+'''
+#Custom override of CSVExporter class
+#Adds functionality to override auto-generated csv headers with custom ones
+#Only exports the x values for the first data item
+'''
+class CustomCSVExporter(pg.exporters.CSVExporter):
+    def __init__(self, item):
+        pg.exporters.CSVExporter.__init__(self, item)
+
+    def _exportPlotDataItem(self, plotDataItem, useX=True) -> None:
+        if hasattr(plotDataItem, 'getOriginalDataset'):
+            # try to access unmapped, unprocessed data
+            cd = plotDataItem.getOriginalDataset()
+        else:
+             # fall back to earlier access method
+            cd = plotDataItem.getData()
+        if cd[0] is None:
+            # no data found, break out...
+            return None
+        
+        if useX:
+            self.data.append(cd[0])
+
+        #Only adding cd[1] so that we don't have x values repeated
+        self.data.append(cd[1])
+
+        index = next(self.index_counter)
+        if plotDataItem.name() is not None:
+            name = plotDataItem.name().replace('"', '""') + '_'
+            xName = f"{name}x"
+            yName = f"{name}y"
+        else:
+            xName = f'x{index:04}'
+            yName = f'y{index:04}'
+        appendAllX = self.params['columnMode'] == '(x,y) per plot'
+        if (appendAllX or index == 0) and useX:
+            self.header.extend([xName, yName])
+        else:
+            self.header.extend([yName])
+        return None
+
+    def export(self, fileName=None, custom_header=None):
+        if not isinstance(self.item, PlotItem.PlotItem):
+            raise TypeError("Must have a PlotItem selected for CSV export.")
+
+        if fileName is None:
+            self.fileSaveDialog(filter=["*.csv", "*.tsv"])
+            return
+
+        needX=True
+
+        for item in self.item.items:
+            if isinstance(item, ErrorBarItem.ErrorBarItem):
+                self._exportErrorBarItem(item)
+            elif hasattr(item, 'implements') and item.implements('plotData'):
+                self._exportPlotDataItem(item,needX)
+                needX=False
+
+        if(custom_header is not None):
+            # print("CUstom")
+            self.header = custom_header
+
+        sep = "," if self.params['separator'] == 'comma' else "\t"
+        # we want to flatten the nested arrays of data into columns
+        # columns = [column for dataset in self.data for column in dataset]
+        columns = [column for column in self.data]
+        with open(fileName, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=sep, quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(self.header)
+            for row in itertools.zip_longest(*columns, fillvalue=""):
+                row_to_write = [
+                    item if isinstance(item, str) 
+                    else np.format_float_positional(
+                        item, precision=self.params['precision']
+                    )
+                    for item in row
+                ]
+                writer.writerow(row_to_write)
+
+        self.header.clear()
+        self.data.clear()
+
+# CustomCSVExporter.register()
--- src/icarus_v2/gui/styled_plot_widget.py
+++ src/icarus_v2/gui/styled_plot_widget.py
@@ -1,23 +1,110 @@
 from pyqtgraph import PlotWidget
+
 from icarus_v2.qdarktheme.load_style import THEME_COLOR_VALUES
+
+import pyqtgraph as pg
+
+from pyqtgraph.graphicsItems.ButtonItem import ButtonItem
+
+from pyqtgraph import icons
+
+from pyqtgraph import PlotItem
+
+# import pyqtgraph.exporters
+
+from icarus_v2.backend.custom_csv_exporter import CustomCSVExporter
+
 
 
 class StyledPlotWidget(PlotWidget):
+
     def __init__(self, x_zoom=False):
-        theme = 'dark'
+
+        theme = 'dark' #TODO: know that this is here
+
         background = THEME_COLOR_VALUES[theme]['background']['base']
+
         self.text_color = THEME_COLOR_VALUES[theme]['foreground']['base']
+
         PlotWidget.__init__(self, background=background)
 
+
         self.showGrid(x=True, y=True)
+
         self.setMouseEnabled(x=x_zoom, y=False)  # Prevent zooming
+
         self.hideButtons()  # Remove autoScale button
 
+        self.getPlotItem().getViewBox().setMenuEnabled(False) # Remove right click menu
+
+        self.exportBtn = ButtonItem(icons.getGraphPixmap('auto'), 14,self.plotItem)
+        self.exportBtn.clicked.connect(self.exportBtnClicked)
+        self.exportBtn.setPos(30,210)
+        self.exportBtn.hide()
+        # self.scene.sigMouseMoved
+
+        self.btn_proxy = pg.SignalProxy(self.scene().sigMouseHover, rateLimit=120, slot=self.hovering)
+
+
+        self.csv_header = None
+
+
     def set_title(self, title):
+
         self.setTitle(title, color=self.text_color, size="17pt")
 
+
     def set_y_label(self, label):
+
+        self.test_label=label
+
         self.setLabel('left', label, **{'color': self.text_color})
 
+
     def set_x_label(self, label):
+
         self.setLabel('bottom', label, **{'color': self.text_color})
+
+    '''
+
+    #Formatting for header is array of strings with x as the first values
+
+    #Ex: ["X","Graph1","Graph2","Graph3"]
+    '''
+
+    def set_csv_header(self,csv_header):
+
+        self.csv_header = csv_header
+
+    def hovering(self):  
+        self.exportBtn.show()
+
+    def exportBtnClicked(self):
+
+        print("Click")
+
+    def export_png(self, filename):
+
+        exporter = pg.exporters.ImageExporter(self.plotItem)
+
+
+        # set export parameters if needed
+
+        #TODO: figure out dimensions for graph
+
+        exporter.parameters()['width'] = 650   # (note this also affects height parameter)
+
+
+        # save to file
+
+        exporter.export(filename+'.png')
+
+
+    def export_csv(self, filename):
+
+        exporter = CustomCSVExporter(self.plotItem)
+
+
+        # save to file
+
+        exporter.export(filename+'.csv',self.csv_header)
