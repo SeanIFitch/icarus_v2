416e07e735c58101e372bdb71fe46cf4c3d165ce
Sean Fitch <seanifitch@gmail.com>
2024-11-01 16:50:12 -0400

    Fix UDEV rules being applied to wrong user

--- src/icarus_v2/utils/udev_setup.py
+++ src/icarus_v2/utils/udev_setup.py
@@ -1,5 +1,6 @@
 import importlib
 import subprocess
+import os
 
 
 # Adds permissions for the user to access the USB device
@@ -7,7 +8,8 @@ def setup_udev_rules():
     with importlib.resources.path('icarus_v2.resources.setup', 'install_udev_rules.sh') as script_path:
         try:
             # Run the script with 'pkexec'
-            command = ['pkexec', str(script_path)]  # Convert PosixPath to string
+            current_user = os.getlogin()
+            command = ['pkexec', '--user', current_user, str(script_path)]  # Convert PosixPath to string
             result = subprocess.run(command, stderr=subprocess.PIPE)
             if result.returncode != 0:
                 raise RuntimeError(f"Command failed: {result.stderr.decode().strip()}")
