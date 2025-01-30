Fix masking off-by-one in period compression resulted in depressurization valve being shown as low when pressurization valve is low

src/icarus_v2/backend/event.py
@@ -70,10 +70,9 @@ def gaussian_filter(data, kernel_size, sigma=1):
     return np.convolve(padded_data, kernel, mode='valid')
 
 
-
 # Represents an event
 # All methods which extract information for plotting are in this class
-class Event():
+class Event:
     PRESSURIZE = 0
     DEPRESSURIZE = 1
     PERIOD = 2
@@ -81,12 +80,12 @@ class Event():
     PUMP = 4
 
     def __init__(self, event_type, data, event_index = None, event_time=None, step_time = None) -> None:
-        if type(event_type) == int and event_type <=4 and event_type >=0:
+        if type(event_type) == int and 4 >= event_type >= 0:
             self.event_type = event_type
         else:
             raise RuntimeError(event_type + "event not supported.")
 
-        if event_time == None:
+        if event_time is None:
             self.event_time = time()
             if data is not None:
                 self.event_time -= data.shape[1] / 4000 # Assumes srate = 4000. 
@@ -103,14 +102,13 @@ class Event():
         else:
             self.step_time = step_time
 
-        # Period events can be long. Therefore only take 600 data points to log and plot. (same as pressurize and depressurize plots)
+        # Period events can be long. Therefore, only take 600 data points to log and plot. (same as pressurize and depressurize plots)
         # No statistical analysis of period events is necessary, so this loss of data is fine.
         # If initialized with a step time, this means the data has already been compressed.
         if self.event_type == self.PERIOD or self.event_type == self.PUMP and step_time is None:
             self.data = self.compress_data(data, 600)
         else:
             self.data = data # np.ndarray (?,8) np.int16
-
 
     # used to call all info functions
     def get_event_info(self, info_type):
@@ -135,7 +133,6 @@ class Event():
         elif info_type == "press sample switch":
             return self.get_sample_switch_time()
 
-
     # Decrease size of data. Takes every nth data point, making sure not to lose valve events.
     def compress_data(self, data, num_points):
         if len(data) <= num_points:
@@ -147,21 +144,20 @@ class Event():
 
             compressed_data = data[indices]
 
-            # Change where event occured
+            # Change where event occurred
             self.event_index = np.round(self.event_index / step).astype(int)
 
             # Recover lost valve events so they are plotted
-            depressurize_indeces = np.argmin(get_channel(data, Channel.DEPRE_VALVE))
-            pressurize_indeces = np.argmin(get_channel(data, Channel.PRE_VALVE))
-            compressed_depressurize_indeces = np.unique((depressurize_indeces / step).astype(int))
-            compressed_pressurize_indeces = np.unique((pressurize_indeces / step).astype(int))
+            depressurize_indices = np.where(get_channel(data, Channel.DEPRE_VALVE) == np.min(get_channel(data, Channel.DEPRE_VALVE)))[0]
+            pressurize_indices = np.where(get_channel(data, Channel.PRE_VALVE) == np.min(get_channel(data, Channel.PRE_VALVE)))[0]
+            compressed_depressurize_indices = np.unique((depressurize_indices / step).astype(int))
+            compressed_pressurize_indices = np.unique((pressurize_indices / step).astype(int))
             # Set to false with bitwise operation
-            depre_mask = np.uint16(0b1111111111111011)
-            pre_mask = np.uint16(0b1111111111110111)
-            compressed_data[compressed_depressurize_indeces,7] = compressed_data[compressed_depressurize_indeces,7] & depre_mask
-            compressed_data[compressed_pressurize_indeces,7] = compressed_data[compressed_pressurize_indeces,7] & pre_mask
+            depre_mask = np.uint16(0b1111111111111101)
+            pre_mask = np.uint16(0b1111111111111011)
+            compressed_data[compressed_depressurize_indices,7] = compressed_data[compressed_depressurize_indices,7] & depre_mask
+            compressed_data[compressed_pressurize_indices,7] = compressed_data[compressed_pressurize_indices,7] & pre_mask
             return compressed_data
-
 
     # Average of entire event
     def get_origin_pressure(self):
@@ -180,44 +176,6 @@ class Event():
         target_pressure = np.average(get_channel(self, Channel.TARGET))
         return target_pressure
 
-
-    # Returns time valve was open in terms of indeces, so divide result by device sample rate for time
-    def get_valve_open_time(self):
-        if self.event_type == self.PRESSURIZE:
-            valve = Channel.PRE_VALVE
-        elif self.event_type == self.DEPRESSURIZE:
-            valve = Channel.DEPRE_VALVE
-        else:
-            raise RuntimeError("Cannot call get_valve_open_time() on event type " + self.event_type)
-
-        # find duration of event
-        duration = np.argmax(get_channel(self, valve)[self.event_index:])
-        # Case where the end of the event is not in the reported data
-        if get_channel(self, valve)[self.event_index + duration] == False:
-            duration = len(self.data) - self.event_index
-        return duration
-
-
-    # Returns period duration in terms of indeces, so divide result by device sample rate for time
-    def get_period_width(self):
-        if self.event_type != self.PERIOD:
-            raise RuntimeError("Cannot call get_period_width() on event type " + self.event_type) 
-
-        # There is a buffer of equivalent size on either end of the data
-        width = len(self.data) - 2 * self.event_index
-        return width
-
-
-    # Returns delay duration in terms of indeces, so divide result by device sample rate for time
-    def get_delay_width(self):
-        if self.event_type != self.PERIOD:
-            raise RuntimeError("Cannot call get_delay_width() on event type " + self.event_type) 
-
-        # Find length of delay
-        delay = np.argmin(get_channel(self, Channel.PRE_VALVE)[self.event_index:])
-        return delay
-
-
     # Returns pressure before event starts
     def get_initial_sample(self):
         if self.event_type != self.DEPRESSURIZE:
@@ -225,7 +183,6 @@ class Event():
 
         sample_pressure = np.average(get_channel(self, Channel.HI_PRE_SAMPLE)[:self.event_index])
         return sample_pressure
-
 
     # Returns pressure before event starts
     def get_initial_origin(self):
@@ -235,7 +192,6 @@ class Event():
         origin_pressure = np.average(get_channel(self, Channel.HI_PRE_ORIG)[:self.event_index])
         return origin_pressure
 
-
     # Returns pressure before event starts
     def get_initial_target(self):
         if self.event_type != self.PUMP:
@@ -243,7 +199,6 @@ class Event():
 
         target_pressure = np.average(get_channel(self, Channel.TARGET)[:self.event_index])
         return target_pressure
-
 
     # Returns difference of index of half max and event_index
     def get_origin_switch_time(self):
@@ -258,7 +213,6 @@ class Event():
 
         return half_max_index - self.event_index
 
-
     # Returns difference of index of half max and event_index
     def get_sample_switch_time(self):
         if self.event_type != self.DEPRESSURIZE and self.event_type != self.PRESSURIZE:
@@ -272,14 +226,11 @@ class Event():
 
         return half_max_index - self.event_index
 
-
     def get_origin_slope(self):
         return self.get_slope(Channel.HI_PRE_ORIG)
 
-
     def get_sample_slope(self):
         return self.get_slope(Channel.HI_PRE_SAMPLE)
-
 
     # Returns max slope of pressurization or depressurization
     def get_slope(self, channel):